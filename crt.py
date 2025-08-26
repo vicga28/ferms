43b74ecebbb51da4d17cf46338cb7b6f213acc6d
vicga28 <97985206+vicga28@users.noreply.github.com>
2025-08-26 11:25:28 +0200

    Update crt.py

--- crt.py
+++ crt.py
@@ -6,7 +6,7 @@ import io
 ### Aquest script serveix per obtenir un llistat dels trams de carretera per sota un valor llindar de CRT per així poder determinar quins trams seran subjecte de reparacions puntuals
 
 # Càrrega d'arxiu amb valors CRT
-df = pd.read_csv('CRT.csv', delimiter=";")
+df = pd.read_csv('CRT_2.csv', delimiter=";")
 
 # Adaptació dels valors de l'arxiu per al seu tractament
 df['PKI'] = pd.to_numeric(df['PKI'], errors='coerce')
@@ -140,4 +140,5 @@ if submitted:
     excel = convert_for_download(df_result)
     st.download_button(label='Descarregar fitxer', data=excel, file_name='resultats.xlsx', mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
                        icon=":material/download:")
+
 
